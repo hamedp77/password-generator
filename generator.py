5a0432b2b9e3661445b074628a7c3f55a5c78a5e
Hamed Pourkhaleghi <hamedp77@gmail.com>
2023-03-05 13:52:19 +0330

    added basic argument parsing

--- generator.py
+++ generator.py
@@ -3,12 +3,26 @@ import random
 import string
 # from time import perf_counter
 
-INCLUDE_DIGITS = True
-INCLUDE_SYMBOLS = True
-INCLUDE_UPPERCASE = True
+parser = argparse.ArgumentParser(
+    description='A python CLI tool for generating random passwords ')
+
+parser.add_argument('-l', '--length', default=8, type=int,
+                    nargs='?', help='Length of the generated password (between 5-32)')
+parser.add_argument('--no-digit', action='store_false',
+                    dest='digits_allowed', help='Do not use digits in the password')
+parser.add_argument('--no-symbol', action='store_false',
+                    dest='symbols_allowed', help='Do not use symbols in the password')
+parser.add_argument('--no-upper', action='store_false', dest='upper_allowed',
+                    help='Do not use uppercase letters in the password')
+
+args = parser.parse_args()
+
+INCLUDE_DIGITS = args.digits_allowed
+INCLUDE_SYMBOLS = args.symbols_allowed
+INCLUDE_UPPERCASE = args.upper_allowed
 
 
-def generator(length: int = 8) -> str:
+def generator(length: int) -> str:
     """"Generate a random password of specified length and characters.
 
 
@@ -55,4 +69,4 @@ def check_min_symbols(password: str) -> bool:
 
 
 for i in range(5):
-    print(generator(32))
+    print(generator(args.length))
