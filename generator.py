5349691d3e9898095a13cc004d154544c070bb82
Hamed Pourkhaleghi <hamedp77@gmail.com>
2023-03-06 20:08:03 +0330

    minimum length in the help menu

--- generator.py
+++ generator.py
@@ -97,7 +97,7 @@ def main():
         description='A python CLI tool for generating random passwords ')
 
     parser.add_argument('-l', '--length', type=int, default=8,
-                        nargs='?', help='Length of the generated password (default=8)')
+                        nargs='?', help='Length of the generated password (>=4, default=8)')
     parser.add_argument('-c', '--count', type=int, default=1, nargs='?',
                         help='Number of passwords to be generated (default=1)')
     parser.add_argument('--no-digit', action='store_false',
